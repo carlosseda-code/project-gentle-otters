14ea5429d3a76cde3c2b8ba43dade88b1276af5c
Carlos Seda <carlosseda123@gmail.com>
2022-06-18 16:56:17 -0500

    My information

--- app/information.py
+++ app/information.py
@@ -1,14 +1,14 @@
 info = {
     # Your first and last name
-    'name' : 'STUDENT NAME',
+    'name' : 'Carlos Seda',
     # The name of your university
-    'university' : 'University Name',
+    'university' : 'Tecnológico de Monterrey',
     # Information about you
-    'about_yourself' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'about_yourself2' : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit",
+    'about_yourself' : "I am a computer science student who faithfully believes that technology is very powerful and is important for evolution. Developing, innovating and learning is what I do daily. I like to be aware of new technologies and everything new that technology companies create every day.",
+    'about_yourself2' : "I love being involved in events, courses, workshops, internships and everything that has to do with technology. I like to train my code by participating in programming tournaments and challenge myself solving high-level problems.",
     # Education timeline (Follow the format)
     'education' : ['University', 'High school'],
-    'education_names' : ['University Name', 'High school Name'],
+    'education_names' : ['Tecnológico de Monterrey', 'John Spark'],
     'education_gpa' : ['96/100', '98/100'],
     'education_graduation' : ['December 2023'],
     'education_duration' : ['2019 - 2023', '2016 - 2019'],
